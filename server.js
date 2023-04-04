6c352ed8fe6a48b5f6f27e627c24473dcc2bdc4a
Guss-Alves <gualvesmagalhaes2020@gmail.com>
2023-04-04 15:16:01 -0700

    updates to deploy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const localPort = 8000;
 
 require('dotenv').config();
-
+const path = require('path');
 
 app.use( express.json() );
 app.use( express.urlencoded({ extended: true }) ); // need these 2 lines in order to use .post method
@@ -16,5 +16,11 @@ require("./server/config/mongoose.config");
 require('./server/routes/user.routes')(app);
 require('./server/routes/post.routes')(app);
 
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    })
+}
 
 app.listen(process.env.PORT || localPort, () => console.log(`Listening on port: ${localPort}`) );
